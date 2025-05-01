10132ca43c4979c89b17305d4bf7e7f218c8cfdb
hermesDJ04 <hermesmeka8@gmail.com>
2025-05-01 11:29:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 const ADMIN_PASSWORD = 'admin123';
 
